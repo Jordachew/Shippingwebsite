3b0e0e1b5ce7cc571549ab53521e05abab5f7a01
Jordachew <86542421+Jordachew@users.noreply.github.com>
2026-02-04 15:35:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const INVOICE_BUCKET = "invoices";
 
+
 // ========================
 // RATES (EDIT)
 // ========================
