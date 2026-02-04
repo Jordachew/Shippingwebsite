55dcd28f9d1f9f25ceddd981242497db4dac01e5
Jordachew <86542421+Jordachew@users.noreply.github.com>
2026-02-04 15:33:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // SUPABASE CONFIG
 // ========================
 const SUPABASE_URL = "https://ykpcgcjudotzakaxgnxh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_i99PVgfeQkRvtjnemX6V9w_wd97XPng";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcGNnY2p1ZG90emFrYXhnbnhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMTQ5ODMsImV4cCI6MjA4NTc5MDk4M30.PPh1QMU-eUI7N7dy0W5gzqcvSod2hKALItM7cyT0Gt8";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const INVOICE_BUCKET = "invoices";
